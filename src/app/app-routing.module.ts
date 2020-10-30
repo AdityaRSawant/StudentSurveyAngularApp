29c816723af0778c9edd28cf12c76860f33ad69e
AdityaRSawant <adityarsawant@yahoo.com>
2020-10-30 08:18:00 -0400

    First Commit of Angular Project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { StudentSurveyComponent } from './student-survey/student-survey.component';
+import { AllSurveysComponent } from './all-surveys/all-surveys.component';
 
-const routes: Routes = [];
+//Module that holds all the routing parameters with the paths and mapped components for navigation
+const routes: Routes = [
+  { path: '', component: StudentSurveyComponent},
+  { path: 'allsurvey', component: AllSurveysComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
